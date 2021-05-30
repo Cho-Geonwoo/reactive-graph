e9befff48711e5b8f3fff136ebf0093df89a5ff3
Cho-Geonwoo <geonwoocho@algorima.io>
2021-05-30 12:58:07 +0900

    feat: apply typescript

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,8 @@ import { store } from './redux/helper';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
