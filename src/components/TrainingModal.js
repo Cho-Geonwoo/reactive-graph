c5dfa13d70b303bdad98a6c10c5da203e570544e
ChoGeonwoo <53969314+Cho-Geonwoo@users.noreply.github.com>
2021-06-02 01:54:13 +0900

    feat(TrainingModal.js): Hide modal button is added (#7)

--- src/components/TrainingModal.js
+++ src/components/TrainingModal.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Default } from 'react-awesome-spinners';
 import {
   ModalWrapper,
+  DefaultWrapper,
   OnTrainingText,
   MainWrapper,
+  MainWrapperTwo,
   RobotImg,
+  CompressImg,
 } from '../styles/styles';
 
 const TrainingModal = () => {
   const onTrain = useSelector((store) => store.answer.onTrain);
+  const [minimized, setMinimized] = useState(false);
+  const [hideModal, setHideModal] = useState(false);
+
+  useEffect(() => {
+    if (minimized) {
+      setTimeout(() => setHideModal(true), 800);
+    }
+  }, [minimized]);
+
+  // training이 끝나면 minimize mode를 해제시켜 modal을 원래 상태로 복귀시킵니다.
+  useEffect(() => {
+    if (!onTrain) {
+      setMinimized(false);
+    }
+  }, [onTrain]);
 
   // redux store에 있는 상태가 학습 중이면, training modal을 띄우는 부분입니다.
   return (
-    onTrain && (
+    onTrain &&
+    (!hideModal ? (
       <MainWrapper>
-        <ModalWrapper>
+        <ModalWrapper minimized={minimized}>
+          <CompressImg
+            src={`${process.env.PUBLIC_URL}/images/compress.png`}
+            alt="compress"
+            onClick={() => {
+              setMinimized(true);
+            }}
+          />
           <RobotImg
             src={`${process.env.PUBLIC_URL}/images/robot.png`}
             alt="robot"
@@ -24,7 +50,15 @@ const TrainingModal = () => {
           <Default />
         </ModalWrapper>
       </MainWrapper>
-    )
+    ) : (
+      <MainWrapperTwo>
+        <DefaultWrapper>
+          <div style={{ paddingTop: '30px', paddingRight: '30px' }}>
+            <Default />
+          </div>
+        </DefaultWrapper>
+      </MainWrapperTwo>
+    ))
   );
 };
 
