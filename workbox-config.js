9b20f1518b90534fa70ad7c4f3ce5c556339183a
Cho-Geonwoo <geonwoocho@algorima.io>
2021-06-01 15:36:23 +0900

    feat: serviceworker

---
+++ workbox-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  globDirectory: 'build/',
+  globPatterns: ['**/*.{json,ico,html,png,txt,css,js}'],
+  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+  swDest: 'build/service-worker.js',
+};
