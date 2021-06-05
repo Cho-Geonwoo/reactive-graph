139de5c21c9349b25ca8e3cd1d8d452ba95ccd98
Cho-Geonwoo <geonwoocho@algorima.io>
2021-06-05 21:16:17 +0900

    chore: remove html from serviceWorkerlist

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   globDirectory: 'build/',
-  globPatterns: ['**/*.{json,ico,html,png,txt,css,js}'],
+  globPatterns: ['**/*.{json,ico,png,txt,css,js}'],
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
   swDest: 'build/service-worker.js',
 };
